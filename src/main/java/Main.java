d7218c36705825bcb07b7bf91711e34884387fa7
VKalyadin <vkalyadin87@gmail.com>
2023-01-27 14:56:32 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        BonusService service = new BonusService();
+        long bonus = service.calculate(500, true);
+        System.out.println(bonus);
+    }
+}
